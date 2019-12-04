d46cd0d11da3cdb1fc7bbdb121996f02b0426a4b
Shree <sajalkomer@gmail.com>
2019-12-03 19:50:57 -0500

    added functionality

---
+++ src/main/java/SumNumber.java
@@ -0,0 +1,6 @@
+public class SumNumber {
+    public static int sumTwoNumber(int x, int y){
+        int sum = x+y;
+        return sum;
+    }
+}
---
+++ src/test/java/SumNumberTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumNumberTest {
+
+    @Test
+    void sumTwoNumber() {
+        assertEquals(5, SumNumber.sumTwoNumber(2,3));
+    }
+}
